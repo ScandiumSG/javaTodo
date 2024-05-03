a46bb39d3c8b768288391ea071e3d6775e3c4467
Stian Gaustad <stian.gaustad@no.experis.com>
2024-05-03 15:15:38 +0200

    PageHeader - Added some header content

--- todoapp/src/app/page-header/page-header.component.ts
+++ todoapp/src/app/page-header/page-header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataFetcherService } from '../services/data-fetcher.service';
 
 @Component({
   selector: 'app-page-header',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './page-header.component.css'
 })
 export class PageHeaderComponent {
+  totalTasks: number = 0;
+  incompleteTasks: number = 0;
 
+  constructor (private data: DataFetcherService) { 
+    this.data.numberOfTasks.subscribe((value: number) => {
+      this.totalTasks = value;
+    })
+    this.data.numberOfIncompleteTasks.subscribe((value: number) => {
+      this.incompleteTasks = value;
+    })
+  }
 }
--- todoapp/src/app/services/data-fetcher.service.ts
+++ todoapp/src/app/services/data-fetcher.service.ts
@@ -10,12 +10,16 @@ import { error } from 'node:console';
 
 export class DataFetcherService {
   public recievedData: BehaviorSubject<Task[]> =  new BehaviorSubject<Task[]>([]);
+  public numberOfTasks: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  public numberOfIncompleteTasks: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
   constructor(private http: HttpClient) {
     this.http.get<Task[]>('http://localhost:8080/task/v2/')
     .pipe(
       tap(tasks => {
         this.recievedData.next(tasks);
+        this.numberOfTasks.next(tasks.length);
+        this.numberOfIncompleteTasks.next(tasks.filter(task => task.completed).length);
       })
     )
     .subscribe({
