cb042f9a17ad0247a2b29fdd645a182595ce2a5e
Stian Gaustad <stian.gaustad@no.experis.com>
2024-05-03 14:52:52 +0200

    Frontend - Fixed error connections on docker host

--- todoapp/src/app/services/data-fetcher.service.ts
+++ todoapp/src/app/services/data-fetcher.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, tap } from 'rxjs';
 import { Task } from '../utils/interfaces';
+import { error } from 'node:console';
 
 @Injectable({
   providedIn: 'any'
@@ -11,17 +12,15 @@ export class DataFetcherService {
   public recievedData: BehaviorSubject<Task[]> =  new BehaviorSubject<Task[]>([]);
 
   constructor(private http: HttpClient) {
-    this.getTasks();
-  }
-
-  getTasks(): void {
     this.http.get<Task[]>('http://localhost:8080/task/v2/')
-      .pipe(
-        tap(tasks => {
-          this.recievedData.next(tasks);
-        })
-      )
-      .subscribe();
+    .pipe(
+      tap(tasks => {
+        this.recievedData.next(tasks);
+      })
+    )
+    .subscribe({
+      error: (e) => console.log(e),
+    });
   }
 
 }
