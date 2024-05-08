aab34d6920469a243d51048cde77594dac249958
Stian Gaustad <stian.gaustad@no.experis.com>
2024-05-08 12:44:54 +0200

    Backend - Added more seed data

--- api/src/main/java/todo/api/Data/DatabaseSeeder.java
+++ api/src/main/java/todo/api/Data/DatabaseSeeder.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -19,31 +22,59 @@ public class DatabaseSeeder implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         // Insert seed data
-        Task task1 = new Task(
-            1, 
-            "Hygiene", 
-            "Take a shower and brush your teeth", 
-            true, 
-            LocalDateTime.of(LocalDate.of(2023, 3, 20), LocalTime.now().truncatedTo(ChronoUnit.SECONDS)), 
-            LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
-        Task task2 = new Task(
-            2, 
-            "Chores", 
-            "Load and start the dishwasher", 
-            false, 
-            LocalDateTime.of(LocalDate.of(2023, 3, 22), LocalTime.now().truncatedTo(ChronoUnit.SECONDS)), 
-            LocalDateTime.of(LocalDate.of(2023, 3, 22), LocalTime.now()).truncatedTo(ChronoUnit.SECONDS));
-        Task task3 = new Task(
-            3, 
-            "Pay bills", 
-            "Approve invoices and transfer money to food account", 
-            false, 
-            LocalDateTime.of(LocalDate.of(2023, 4, 15), LocalTime.now().truncatedTo(ChronoUnit.SECONDS)), 
-            LocalDateTime.of(LocalDate.of(2023, 4, 15), LocalTime.now()).truncatedTo(ChronoUnit.SECONDS));
+        List<Task> tasks = new ArrayList<Task>(); 
 
-        taskRepo.save(task1);
-        taskRepo.save(task2);
-        taskRepo.save(task3);
+        tasks.add(new Task(
+                1, 
+                "Hygiene", 
+                "Take a shower and brush your teeth", 
+                true, 
+                LocalDateTime.of(LocalDate.of(2023, 3, 20), LocalTime.now().truncatedTo(ChronoUnit.SECONDS)), 
+                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)
+            )
+        );
+
+        tasks.add(new Task(
+                2, 
+                "Chores", 
+                "Load and start the dishwasher", 
+                false, 
+                LocalDateTime.of(LocalDate.of(2023, 3, 22), LocalTime.now().truncatedTo(ChronoUnit.SECONDS)), 
+                LocalDateTime.of(LocalDate.of(2023, 3, 22), LocalTime.now()).truncatedTo(ChronoUnit.SECONDS)
+            )
+        );
+
+        tasks.add(new Task(
+                3, 
+                "Pay bills", 
+                "Approve invoices and transfer money to food account", 
+                false, 
+                LocalDateTime.of(LocalDate.of(2023, 4, 15), LocalTime.now().truncatedTo(ChronoUnit.SECONDS)), 
+                LocalDateTime.of(LocalDate.of(2023, 4, 15), LocalTime.now()).truncatedTo(ChronoUnit.SECONDS)
+            )
+        );
+
+        tasks.add(new Task(
+                4,
+                "Paint wall",
+                "Paint the south wall green, and the west wall yellow",
+                false,
+                LocalDateTime.of(LocalDate.of(2023, 6, 20), LocalTime.now().truncatedTo(ChronoUnit.SECONDS)), 
+                LocalDateTime.of(LocalDate.of(2023, 6, 25), LocalTime.now()).truncatedTo(ChronoUnit.SECONDS)
+            )
+        );
+
+        tasks.add(new Task(
+            5,
+            "Take out trash",
+            "Empty the trash bin",
+            false,
+            LocalDateTime.of(LocalDate.of(2024, 2, 13), LocalTime.now().truncatedTo(ChronoUnit.SECONDS)), 
+            LocalDateTime.of(LocalDate.of(2024, 2, 13), LocalTime.now()).truncatedTo(ChronoUnit.SECONDS)
+        )
+    );
+
+        taskRepo.saveAll(tasks);
 
         // Add more seed data as needed
         System.out.println(
